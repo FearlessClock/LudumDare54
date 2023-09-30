f69642260676adf79fe0cec91597684bd66cc750
MrEsport <lucasespositonc@gmail.com>
2023-09-30 13:25:23 +0200

    [ADD] Item class and prefab
    * Item data contains info
    * Block Layout decides the Size and Sprite Offset
    * Click+Drag moves the object, centered around its "pivot block"

--- Assets/1 Objects/Block/Block.cs
+++ Assets/1 Objects/Block/Block.cs
@@ -20,8 +20,14 @@ public class Block : MonoBehaviour
 
     [SerializeField] private bool pickable = true;
 
-    private void Start()
+    protected List<Vector2Int> blockLayoutOffset;
+
+    public Vector2 BlockLayoutSize { get; private set; }
+    public Vector2 BlockPivotOffset { get; private set; }
+
+    protected virtual void Start()
     {
+        ComputeBlockLayout();
         if (pickable)
             OnDrag.AddListener(() => MoveTo(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
     }
@@ -44,6 +50,27 @@ public class Block : MonoBehaviour
     public void MoveTo(Vector2 pos)
     {
         transform.position = pos;
+    }
+
+    private void ComputeBlockLayout()
+    {
+        Vector2 offset;
+        Vector2 botLeft = Vector2.zero;
+        Vector2 topRight = Vector2.zero;
+        for (int i = 0; i < blockLayoutOffset.Count; ++i)
+        {
+            offset = blockLayoutOffset[i];
+            if (offset.x < botLeft.x) botLeft.x = offset.x;
+            if (offset.y < botLeft.y) botLeft.y = offset.y;
+            if (offset.x > topRight.x) topRight.x = offset.x;
+            if (offset.y > topRight.y) topRight.y = offset.y;
+        }
+
+        BlockPivotOffset = (botLeft + topRight) / 2f;
+
+        botLeft.x = Mathf.Abs(botLeft.x);
+        botLeft.y = Mathf.Abs(botLeft.y);
+        BlockLayoutSize = botLeft + topRight + Vector2.one; // * cellSize
     }
 
     private void OnDestroy()
