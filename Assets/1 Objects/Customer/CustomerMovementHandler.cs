3a67c6972fa29d3d2901e64808fa6e13346eb5d2
Luca Schifano <luca.schifano005@gmail.com>
2023-09-30 17:43:55 +0200

    Spawn customer with random items

--- Assets/1 Objects/Customer/CustomerMovementHandler.cs
+++ Assets/1 Objects/Customer/CustomerMovementHandler.cs
@@ -102,6 +102,7 @@ public class CustomerMovementHandler : MonoBehaviour
         else
         {
             hasPath = false;
+            Debug.Log("end");
         }
     }
 
--- Assets/1 Objects/Customer/CustomerSpawner.cs
+++ Assets/1 Objects/Customer/CustomerSpawner.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CustomerSpawner : MonoBehaviour
@@ -8,13 +10,37 @@ public class CustomerSpawner : MonoBehaviour
     [SerializeField] private Transform entrancePoint = null;
     [SerializeField] private Transform exitPoint = null;
     [SerializeField] private ItemAStarTargetPoints[] targets = null;
+    
 
-    private IEnumerator Start()
+    /*private IEnumerator Start()
     {
         yield return new WaitForEndOfFrame();
 
         CustomerMovementHandler customer = Instantiate<CustomerMovementHandler>(customerPrefab, entrancePoint.position, entrancePoint.rotation, transform);
 
         customer.Init(targets, exitPoint.position);
+    }*/
+
+    public IEnumerator Spawn(int itemToTake)
+    {
+        yield return new WaitForEndOfFrame();
+
+        CustomerMovementHandler customer = Instantiate<CustomerMovementHandler>(customerPrefab, entrancePoint.position, entrancePoint.rotation, transform);
+
+        ItemAStarTargetPoints[] _targets = new ItemAStarTargetPoints[itemToTake];
+
+        for (int i = 0; i < _targets.Length; i++)
+        {
+            var item = targets[UnityEngine.Random.Range(1, targets.Length)];
+
+            while(_targets.Contains(item))
+            {
+                item = targets[UnityEngine.Random.Range(1, targets.Length)];
+            }
+
+            _targets[i] = item;
+        }
+
+        customer.Init(_targets, exitPoint.position);
     }
 }
